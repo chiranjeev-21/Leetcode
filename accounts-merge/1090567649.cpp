4e2189efe162187fd1a3493d9584680e823b0cd7
chiranjeev-21 <chiranjeev21012003@gmail.com>
2026-02-24 09:10:01 +0000

    Add 4 LeetCode solutions

---
+++ accounts-merge/1090567649.cpp
@@ -0,0 +1,113 @@
+
+/*
+==========================================================
+Problem: Accounts Merge
+Difficulty: Medium
+==========================================================
+
+Given a list of accounts where each element accounts[i] is a list of strings, where the first element accounts[i][0] is a name, and the rest of the elements are emails representing emails of the account.
+
+Now, we would like to merge these accounts. Two accounts definitely belong to the same person if there is some common email to both accounts. Note that even if two accounts have the same name, they may belong to different people as people could have the same name. A person can have any number of accounts initially, but all of their accounts definitely have the same name.
+
+After merging the accounts, return the accounts in the following format: the first element of each account is the name, and the rest of the elements are emails in sorted order. The accounts themselves can be returned in any order.
+
+ 
+Example 1:
+
+
+Input: accounts = [["John","johnsmith@mail.com","john_newyork@mail.com"],["John","johnsmith@mail.com","john00@mail.com"],["Mary","mary@mail.com"],["John","johnnybravo@mail.com"]]
+Output: [["John","john00@mail.com","john_newyork@mail.com","johnsmith@mail.com"],["Mary","mary@mail.com"],["John","johnnybravo@mail.com"]]
+Explanation:
+The first and second John's are the same person as they have the common email "johnsmith@mail.com".
+The third John and Mary are different people as none of their email addresses are used by other accounts.
+We could return these lists in any order, for example the answer [['Mary', 'mary@mail.com'], ['John', 'johnnybravo@mail.com'], 
+['John', 'john00@mail.com', 'john_newyork@mail.com', 'johnsmith@mail.com']] would still be accepted.
+
+
+Example 2:
+
+
+Input: accounts = [["Gabe","Gabe0@m.co","Gabe3@m.co","Gabe1@m.co"],["Kevin","Kevin3@m.co","Kevin5@m.co","Kevin0@m.co"],["Ethan","Ethan5@m.co","Ethan4@m.co","Ethan0@m.co"],["Hanzo","Hanzo3@m.co","Hanzo1@m.co","Hanzo0@m.co"],["Fern","Fern5@m.co","Fern1@m.co","Fern0@m.co"]]
+Output: [["Ethan","Ethan0@m.co","Ethan4@m.co","Ethan5@m.co"],["Gabe","Gabe0@m.co","Gabe1@m.co","Gabe3@m.co"],["Hanzo","Hanzo0@m.co","Hanzo1@m.co","Hanzo3@m.co"],["Kevin","Kevin0@m.co","Kevin3@m.co","Kevin5@m.co"],["Fern","Fern0@m.co","Fern1@m.co","Fern5@m.co"]]
+
+
+ 
+Constraints:
+
+
+	1 <= accounts.length <= 1000
+	2 <= accounts[i].length <= 10
+	1 <= accounts[i][j].length <= 30
+	accounts[i][0] consists of English letters.
+	accounts[i][j] (for j > 0) is a valid email.
+
+==========================================================
+*/
+
+
+struct DSU {
+	vector<int> e;
+	DSU(int N) { e = vector<int>(N, -1); }
+
+	int get(int x) { return e[x] < 0 ? x : e[x] = get(e[x]); }
+
+	bool same_set(int a, int b) { return get(a) == get(b); }
+
+	int size(int x) { return -e[get(x)]; }
+
+	bool unite(int x, int y) {
+		x = get(x), y = get(y);
+		if (x == y) return false;
+		if (e[x] > e[y]) swap(x, y);
+		e[x] += e[y];
+		e[y] = x;
+		return true;
+	}
+};
+class Solution {
+public:
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);              
+        cout.tie(0);
+        int n = accounts.size();
+        DSU ds(n);
+        unordered_map<string, int> mp;
+        for(int i=0;i<n;i++){
+            for(int j=1;j<accounts[i].size();j++){
+                string mail = accounts[i][j];
+                if(mp.find(mail) == mp.end()){
+                    mp[mail] = i;
+                }
+                else{
+                    ds.unite(i, mp[mail]);
+                }
+            }
+        }
+
+        vector<string> mergeMail[n];
+        for(auto it: mp){
+            string mail = it.first;
+            int node = ds.get(it.second);
+            mergeMail[node].push_back(mail);
+        }
+        // for(auto i :mergeMail){
+        //     for(auto j :i){
+        //         cout<<j<<" ";
+        //     }
+        //     cout<<"\n";
+        // }
+        vector<vector<string>> ans;
+        for (int i = 0; i < n; i++) {
+            if(mergeMail[i].size() == 0) continue;
+            sort(mergeMail[i].begin(), mergeMail[i].end());
+            vector<string> temp;
+            temp.push_back(accounts[i][0]);
+            for (auto it : mergeMail[i]) {
+                temp.push_back(it);
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
