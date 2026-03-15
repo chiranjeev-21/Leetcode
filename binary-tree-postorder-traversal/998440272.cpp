ff10cfe8e302e5f93065940e84eeb8db972dcb86
chiranjeev-21 <chiranjeev21012003@gmail.com>
2026-03-15 13:59:02 +0000

    Add 4 LeetCode solutions

---
+++ binary-tree-postorder-traversal/998440272.cpp
@@ -0,0 +1,97 @@
+
+/*
+==========================================================
+Problem: Binary Tree Postorder Traversal
+Difficulty: Easy
+==========================================================
+
+Given the root of a binary tree, return the postorder traversal of its nodes' values.
+
+ 
+Example 1:
+
+
+Input: root = [1,null,2,3]
+
+Output: [3,2,1]
+
+Explanation:
+
+
+
+
+Example 2:
+
+
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+
+Output: [4,6,7,5,2,9,8,3,1]
+
+Explanation:
+
+
+
+
+Example 3:
+
+
+Input: root = []
+
+Output: []
+
+
+Example 4:
+
+
+Input: root = [1]
+
+Output: [1]
+
+
+ 
+Constraints:
+
+
+	The number of the nodes in the tree is in the range [0, 100].
+	-100 <= Node.val <= 100
+
+
+ 
+Follow up: Recursive solution is trivial, could you do it iteratively?
+
+==========================================================
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;
+        vector<pair<TreeNode*, bool>> st = {{root, false}};
+        while (!st.empty()) {
+            auto [node, visited] = st.back();
+            st.pop_back();
+            if (node != nullptr)
+                if (visited)
+                    res.push_back(node->val);
+                else {
+                    st.push_back({node, true});
+                    st.push_back({node->right, false});
+                    st.push_back({node->left, false});
+                }
+        }
+        return res;
+    }
+};
