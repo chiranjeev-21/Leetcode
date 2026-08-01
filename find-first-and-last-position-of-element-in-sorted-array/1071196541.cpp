49fb0b13a788a8b92ca9c0396b73037a648efdf7
chiranjeev-21 <chiranjeev21012003@gmail.com>
2026-08-01 20:30:05 +0000

    Add 4 LeetCode solutions

---
+++ find-first-and-last-position-of-element-in-sorted-array/1071196541.cpp
@@ -0,0 +1,81 @@
+
+/*
+==========================================================
+Problem: Find First and Last Position of Element in Sorted Array
+Difficulty: Medium
+==========================================================
+
+Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+If target is not found in the array, return [-1, -1].
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+Example 1:
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+Example 2:
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+Example 3:
+Input: nums = [], target = 0
+Output: [-1,-1]
+
+ 
+Constraints:
+
+
+	0 <= nums.length <= 105
+	-109 <= nums[i] <= 109
+	nums is a non-decreasing array.
+	-109 <= target <= 109
+
+==========================================================
+*/
+
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int n=nums.size();
+        int start=0,end=n-1;
+        int res=-1,res1=-1;
+        if(nums.size()==0){
+            return {-1,-1};
+        }
+        if(nums.size()==1 && nums[0]==target){
+            return {0,0};
+        }
+        while(start<=end){
+            int mid=start + (end-start)/2;
+            if(nums[mid]==target){
+                res=mid;
+                end=mid-1;
+            }
+            else if(nums[mid]<target){
+                start=mid+1;
+            }
+            else if(nums[mid]>target){
+                end=mid-1;
+            }
+        }
+        start=0;
+        end=n-1;
+        while(start<=end){
+            int mid=start + (end-start)/2;
+            if(nums[mid]==target){
+                res1=mid;
+                start=mid+1;
+            }
+            else if(nums[mid]>target){
+                end=mid-1;
+            }
+            else if(nums[mid]<target){
+                start=mid+1;
+            }
+        }
+        // cout<<res<<" ";
+        return {res,res1};
+    }
+};
